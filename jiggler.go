b55f4124a747dfd59b34b2ee08d9a482f1656fca
Cristian <cristian.anghel4@gmail.com>
2025-10-03 13:45:12 +0300

    added flags for speed and human-like movement mode.

--- jiggler.go
+++ jiggler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 	"math"
@@ -13,36 +14,114 @@ import (
 )
 
 const (
+	slow   speed = "s"
+	medium speed = "m"
+	high   speed = "h"
+
 	defaultJiggleTime = 10
+	defaultSpeed      = medium
+)
+
+type speedSetting struct {
+	low   float64
+	high  float64
+	sleep int
+}
+
+type speed string
+
+var (
+	speedConfiguration = map[speed]speedSetting{
+		slow: {
+			low:   1.0,
+			high:  3.0,
+			sleep: 10,
+		},
+		medium: {
+			low:   0.5,
+			high:  1.0,
+			sleep: 10,
+		},
+		high: {
+			low:   0.01,
+			high:  0.05,
+			sleep: 1,
+		},
+	}
 )
 
 func main() {
 	t := flag.Uint("t", defaultJiggleTime, "jiggle time in minutes")
+	s := flag.String("s", string(defaultSpeed), "jiggle speed - slow (s), medium (m), high (h)")
+	h := flag.Bool("h", false, "jiggle human-like (using bezier curve)")
 
 	flag.Parse()
 
-	jiggler := newJiggler(*t)
+	err := validateFlags()
+	if err != nil {
+		log.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	jiggler := newJiggler(*t, speed(*s), *h)
 	if err := jiggler.run(); err != nil {
 		panic(err)
 	}
+}
+
+func validateFlags() error {
+	var err error
+	flag.Visit(func(f *flag.Flag) {
+		switch f.Name {
+		case "s":
+			fs := f.Value.String()
+			_, ok := speedConfiguration[speed(fs)]
+			if !ok && len(fs) > 1 {
+				_, ok = speedConfiguration[speed(fs[0])]
+				if !ok {
+					err = errors.New("invalid value for jiggle speed")
+				}
+			}
+		}
+	})
+	return err
+}
+
+func getSpeedSetting(s speed) speedSetting {
+	v, ok := speedConfiguration[s]
+	if !ok {
+		v = speedConfiguration[speed(s[0])]
+	}
+	return v
 }
 
 type Jiggler struct {
 	screenHeight int
 	screenWidth  int
 	time         uint
+	speed        speedSetting
+	humanLike    bool
 }
 
-func newJiggler(t uint) *Jiggler {
+func newJiggler(t uint, s speed, human bool) *Jiggler {
 	w, h := robotgo.GetScreenSize()
+	speedSetting := getSpeedSetting(s)
+	if !human {
+		(&speedSetting).sleep *= 100
+	}
+
 	return &Jiggler{
 		screenHeight: h,
 		screenWidth:  w,
 		time:         t,
+		speed:        speedSetting,
+		humanLike:    human,
 	}
 }
 
 func (j *Jiggler) run() error {
+	log.Println("press Q at anytime to quit")
 	timer := time.NewTimer(time.Duration(j.time) * time.Minute)
 	go j.stop()
 
@@ -51,8 +130,14 @@ func (j *Jiggler) run() error {
 		case <-timer.C:
 			return nil
 		default:
-			if err := j.jiggleHumanLike(); err != nil {
-				return err
+			if j.humanLike {
+				if err := j.jiggleHumanLike(); err != nil {
+					return err
+				}
+			} else {
+				if err := j.jiggleStraight(); err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -64,19 +149,19 @@ func (j *Jiggler) stop() {
 		if event.Kind == gohook.KeyDown {
 			switch event.Keycode {
 			case gohook.Keycode[robotgo.KeyQ]:
-				os.Exit(1)
+				os.Exit(0)
 			}
 		}
 	}
 }
 
-func (j *Jiggler) jiggle() error {
+func (j *Jiggler) jiggleStraight() error {
 	w := rand.N(j.screenWidth)
 	h := rand.N(j.screenHeight)
-	s := time.Now()
-	log.Printf("moving to %d %d\n", w, h)
-	robotgo.MoveSmooth(w, h, 0.7, 1.1, 500)
-	log.Printf("took %v to move\n", time.Since(s))
+	//s := time.Now()
+	//log.Printf("moving to %d %d\n", w, h)
+	robotgo.MoveSmooth(w, h, j.speed.low, j.speed.high, j.speed.sleep)
+	//log.Printf("took %v to move\n", time.Since(s))
 	return nil
 }
 
@@ -85,22 +170,22 @@ func (j *Jiggler) jiggleHumanLike() error {
 	destX := rand.N(j.screenWidth)
 	destY := rand.N(j.screenHeight)
 
-	log.Printf("starting from %d %d\nfinishing at %d %d\n", startX, startY, destX, destY)
+	//log.Printf("starting from %d %d\nfinishing at %d %d\n", startX, startY, destX, destY)
 
 	p0, p3 := point{startX, startY}, point{destX, destY}
 	p1, p2 := getControlPointsInBbox(p0, p3)
 
-	s := time.Now()
+	//s := time.Now()
 
 	steps := 100
 	for i := 0; i <= steps; i++ {
 		t := float64(i) / float64(steps)
 		curvePoint := cubicBezier(p0, p1, p2, p3, t)
-		log.Printf("moving to %d %d\n", curvePoint.x, curvePoint.y)
+		//log.Printf("moving to %d %d\n", curvePoint.x, curvePoint.y)
 		//determine move speed based on distance to cover. small distance in human-like behaviour means small speed, whereas large distance means higher speed
-		robotgo.MoveSmooth(curvePoint.x, curvePoint.y, 0.01, 0.05, 1)
+		robotgo.MoveSmooth(curvePoint.x, curvePoint.y, j.speed.low, j.speed.high, j.speed.sleep)
 	}
-	log.Printf("took %v to move\n", time.Since(s))
+	//log.Printf("took %v to move\n", time.Since(s))
 	return nil
 }
 
